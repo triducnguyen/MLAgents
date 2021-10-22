859e5ae20eabe1a58c6053979d596ac29a878099
eldersquash <>
2021-10-22 14:28:06 -0600

    ShipController & AlienAgent

---
+++ Hummingbirds/Assets/SpaceGame/Scripts/TrainingManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  TrainingManager: Singleton<TrainingManager>
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
